82c6951c11287d1fa1f45eb21c9bab81eabecb91
maryconnelly <maryconnelly14@gmail.com>
2025-02-15 15:46:05 -0600

    Check win function updated to exclude "" in winning sequences.

--- script.js
+++ script.js
@@ -16,46 +16,55 @@ return { playerOne, playerTwo, activePlayer };
 //Object to control the flow of the game
 const GameController = (function () {
 
-	function makeMove (activePlayer, index) {
+	function makeMove(activePlayer, index) {
 		if (activePlayer === Players.playerOne && Gameboard.boardArray[index] === "") {
 			Gameboard.boardArray[index] = "X";
 		} else if 
 		(activePlayer === Players.playerTwo && Gameboard.boardArray[index] === "") {
 			Gameboard.boardArray[index] = "O";
 		}
-		checkWinner();
-		switchTurns();
-	}
-	makeMove();
+
+	};
 	
-	function checkWinner () {
-		if (Gameboard.boardArray[0] === Gameboard.boardArray[1] && Gameboard.boardArray[1] === Gameboard.boardArray[2] ||
+	function checkWinner() {
+		if (Gameboard.boardArray[0] !== "" && Gameboard.boardArray[1] !== "" && Gameboard.boardArray[2] !== "" && 
+			Gameboard.boardArray[0] === Gameboard.boardArray[1] && Gameboard.boardArray[1] === Gameboard.boardArray[2] ||
+			Gameboard.boardArray[3] !== "" && Gameboard.boardArray[4] !== "" && Gameboard.boardArray[5] !== "" && 
 			Gameboard.boardArray[3] === Gameboard.boardArray[4] && Gameboard.boardArray[4] === Gameboard.boardArray[5] ||
+			Gameboard.boardArray[6] !== "" && Gameboard.boardArray[7] !== "" && Gameboard.boardArray[8] !== "" && 
 			Gameboard.boardArray[6] === Gameboard.boardArray[7] && Gameboard.boardArray[7] === Gameboard.boardArray[8] ||
+			Gameboard.boardArray[0] !== "" && Gameboard.boardArray[3] !== "" && Gameboard.boardArray[6] !== "" && 
 			Gameboard.boardArray[0] === Gameboard.boardArray[3] && Gameboard.boardArray[3] === Gameboard.boardArray[6] ||
+			Gameboard.boardArray[1] !== "" && Gameboard.boardArray[4] !== "" && Gameboard.boardArray[7] !== "" && 
 			Gameboard.boardArray[1] === Gameboard.boardArray[4] && Gameboard.boardArray[4] === Gameboard.boardArray[7] ||
+			Gameboard.boardArray[2] !== "" && Gameboard.boardArray[5] !== "" && Gameboard.boardArray[8] !== "" && 
 			Gameboard.boardArray[2] === Gameboard.boardArray[5] && Gameboard.boardArray[5] === Gameboard.boardArray[8] ||
+			Gameboard.boardArray[0] !== "" && Gameboard.boardArray[4] !== "" && Gameboard.boardArray[8] !== "" && 
 			Gameboard.boardArray[0] === Gameboard.boardArray[4] && Gameboard.boardArray[4] === Gameboard.boardArray[8] ||
-			Gameboard.boardArray[6] === Gameboard.boardArray[4] && Gameboard.boardArray[4] === Gameboard.boardArray[2]) {
-				return winner = `${Players.activePlayer}`;
+			Gameboard.boardArray[6] !== "" && Gameboard.boardArray[4] !== "" && Gameboard.boardArray[2] !== "" && 
+			Gameboard.boardArray[6] === Gameboard.boardArray[4] && Gameboard.boardArray[4] === Gameboard.boardArray[2]) 
 				
+			{ console.log(`${Players.activePlayer} wins!`)
+		
 			} else {
 				switchTurns();
 			}
-		}
+		};
+	
+	
 
-		function switchTurns () {
+		function switchTurns() {
 			if (Players.activePlayer = Players.playerOne) {
 				Players.activePlayer = Players.playerTwo;
 			} else {
 				Players.activePlayer = Players.playerOne;
 			}
-		}
+		};
 
-	return { makeMove, checkWinner, switchTurns }
+		return { makeMove, checkWinner, switchTurns }
 
 	})();
-	
+
 const DisplayController = (function () {
 
 	const container = document.querySelector(".container");
@@ -72,16 +81,24 @@ const DisplayController = (function () {
 				square.id = `square${i + 1}`
 				square.textContent = "";
 				square.addEventListener('click', () => {
-					let index = (i - 1) + 1;
 					if (Players.activePlayer === Players.playerOne) {	
-						GameController.makeMove(Players.playerOne, index);
+						GameController.makeMove(Players.playerOne, (i - 1) + 1);
 						square.textContent = "X";
+						console.log(Gameboard.boardArray);
+						GameController.checkWinner();
+
+				
 						
 					} else if (Players.activePlayer === Players.playerTwo) {
-						GameController.makeMove(Players.playerTwo, index);
+						GameController.makeMove(Players.playerTwo, (i - 1) + 1);
 						square.textContent = "O";
+						console.log(Gameboard.boardArray);
+						GameController.checkWinner();
+						
+			
 					};
 				})
+
 				// if (square.textContent = "") {
 				// 	square.disabled = "false";
 				// } else {
@@ -166,5 +183,4 @@ const DisplayController = (function () {
 return { SidebarDisplay, GameboardDisplay }
 })();
 
-console.log(Gameboard.boardArray);
-console.log(Players.activePlayer);
+console.log(Gameboard.boardArray[0]);
