836335603514be13ee40e3dc8b80f66b59523441
maryconnelly <maryconnelly14@gmail.com>
2025-02-15 14:49:08 -0600

    game play is displayed correctly

--- script.js
+++ script.js
@@ -61,106 +61,107 @@ const DisplayController = (function () {
 	const container = document.querySelector(".container");
 		document.body.appendChild(container);
 	
-	const SidebarDisplay = (function () {
-
-		const sidebar = document.querySelector(".sidebar");
-			container.appendChild(sidebar);
+		const GameboardDisplay = (function () {
 			
-		const turnDisplay = document.querySelector(".turn");
-			sidebar.appendChild(turnDisplay);
-			turnDisplay.textContent = `${Players.activePlayer}'s turn`;
-
-		const form = document.querySelector(".form");
-			sidebar.appendChild(form);
-			form.id = "form";
-
-		const playerOneLabel = document.createElement("label");
-			form.appendChild(playerOneLabel);
-				playerOneLabel.htmlFor = "player-one-input";
-				playerOneLabel.textContent = "Player One's Name:"
-				playerOneLabel.classList.add("player-one");
-				playerOneLabel.id = "player-one-label";
-		const playerOneInput = document.createElement("input");
-			form.appendChild(playerOneInput);
-				playerOneInput.type = "text";
-				playerOneInput.id = "player-one-input";
-				playerOneInput.classList.add("player-one");
-				playerOneInput.required = true;
-
-		const playerTwoLabel = document.createElement("label");
-			form.appendChild(playerTwoLabel);
-				playerTwoLabel.htmlFor = "player-two-input";
-				playerTwoLabel.textContent = "Player Two's Name:"
-				playerTwoLabel.classList.add("player-two");
-				playerTwoLabel.id = "player-two-label";
-		const playerTwoInput = document.createElement("input");
-			form.appendChild(playerTwoInput);
-				playerTwoInput.type = "text";
-				playerTwoInput.id = "player-two-input";
-				playerTwoInput.classList.add("player-two");
-				
-				playerTwoInput.required = true;
-
-		const playButton = document.createElement("submit");
-			form.appendChild(playButton);
-			playButton.textContent = "Play Tic-Tac-Toe!";
-			playButton.id = "play-button";
-			playButton.addEventListener('click', () => {
-				playerOneInput.style.display = "none";
-				playerOneLabel.textContent = `X: ${playerOneInput.value}`;
-				playerTwoInput.style.display = "none";
-				playerTwoLabel.textContent = `O: ${playerTwoInput.value}`;
-				playButton.style.display = "none";
-				turnDisplay.style.display = "block";
-
-			const playAgainButton = document.createElement("button");
-				sidebar.appendChild(playAgainButton);
-				playAgainButton.id = "play-again";
-				playAgainButton.textContent = "Play Again?";
-				playAgainButton.type = "button";
-				playAgainButton.addEventListener('click', () => {
-					Gameboard.boardArray = [ "", "", "", "", "", "", "", "", "" ];
-					DisplayController[GameboardDisplay].square.textContent = "";
-					Players.playerOneInput.style.display = "block";
-					Players.playerTwoInput.style.display = "block";
-					DisplayController[SidebarDisplay].playButton.style.display = "block";
+			const boardDisplay = document.querySelector(".board");
+			container.appendChild(boardDisplay);
+			
+			for (let i=0;i<9;i++) {
+			const square = document.createElement("button");
+				boardDisplay.appendChild(square);
+				square.id = `square${i + 1}`
+				square.textContent = "";
+				square.addEventListener('click', () => {
+					let index = (i - 1) + 1;
+					if (Players.activePlayer === Players.playerOne) {	
+						GameController.makeMove(Players.playerOne, index);
+						square.textContent = "X";
+						
+					} else if (Players.activePlayer === Players.playerTwo) {
+						GameController.makeMove(Players.playerTwo, index);
+						square.textContent = "O";
+					};
 				})
-			})
+				// if (square.textContent = "") {
+				// 	square.disabled = "false";
+				// } else {
+				// 	square.disabled = "true";
+				// }
+			};
 
-			return { sidebar, turnDisplay, playerOneInput, playerOneLabel, playerTwoInput, playerTwoLabel }	
-			
+			return { container, boardDisplay  }
 		})();
-
-	const GameboardDisplay = (function () {
 		
-		const boardDisplay = document.querySelector(".board");
-		container.appendChild(boardDisplay);
-		
-		for (let i=0;i<9;i++) {
-		const square = document.createElement("button");
-			boardDisplay.appendChild(square);
-			square.id = `square${i + 1}`
-			square.textContent = "";
-			square.addEventListener('click', () => {
-				let index = (i - 1) + 1;
-				if (Players.activePlayer === Players.playerOne) {	
-					GameController.makeMove(Players.playerOne, index);
-					square.textContent = "X";
-					
-				} else if (Players.activePlayer === Players.playerTwo) {
-					GameController.makeMove(Players.playerTwo, index);
-					square.textContent = "O";
-				};
-			})
-			// if (square.textContent = "") {
-			// 	square.disabled = "false";
-			// } else {
-			// 	square.disabled = "true";
-			// }
-		};
-			
-	return { container, boardDisplay  }
-	})();
+		const SidebarDisplay = (function () {
+	
+			const sidebar = document.querySelector(".sidebar");
+				container.appendChild(sidebar);
+				
+			const turnDisplay = document.querySelector(".turn");
+				sidebar.appendChild(turnDisplay);
+				turnDisplay.textContent = `${Players.activePlayer}'s turn`;
+	
+			const form = document.querySelector(".form");
+				sidebar.appendChild(form);
+				form.id = "form";
+	
+			const playerOneLabel = document.createElement("label");
+				form.appendChild(playerOneLabel);
+					playerOneLabel.htmlFor = "player-one-input";
+					playerOneLabel.textContent = "Player One's Name:"
+					playerOneLabel.classList.add("player-one");
+					playerOneLabel.id = "player-one-label";
+			const playerOneInput = document.createElement("input");
+				form.appendChild(playerOneInput);
+					playerOneInput.type = "text";
+					playerOneInput.id = "player-one-input";
+					playerOneInput.classList.add("player-one");
+					playerOneInput.required = true;
+	
+			const playerTwoLabel = document.createElement("label");
+				form.appendChild(playerTwoLabel);
+					playerTwoLabel.htmlFor = "player-two-input";
+					playerTwoLabel.textContent = "Player Two's Name:"
+					playerTwoLabel.classList.add("player-two");
+					playerTwoLabel.id = "player-two-label";
+			const playerTwoInput = document.createElement("input");
+				form.appendChild(playerTwoInput);
+					playerTwoInput.type = "text";
+					playerTwoInput.id = "player-two-input";
+					playerTwoInput.classList.add("player-two");
+					playerTwoInput.required = true;
+	
+			const playButton = document.createElement("submit");
+				form.appendChild(playButton);
+				playButton.textContent = "Play Tic-Tac-Toe!";
+				playButton.id = "play-button";
+				playButton.addEventListener('click', () => {
+					playerOneInput.style.display = "none";
+					playerOneLabel.textContent = `X: ${playerOneInput.value}`;
+					playerTwoInput.style.display = "none";
+					playerTwoLabel.textContent = `O: ${playerTwoInput.value}`;
+					playButton.style.display = "none";
+					turnDisplay.style.display = "block";
+					sidebar.style.gridArea = "1 / 1 / 3 / 2";
+					GameboardDisplay.boardDisplay.style.display = "grid";
+	
+				// const playAgainButton = document.createElement("button");
+				// 	sidebar.appendChild(playAgainButton);
+				// 	playAgainButton.id = "play-again";
+				// 	playAgainButton.textContent = "Play Again?";
+				// 	playAgainButton.type = "button";
+				// 	playAgainButton.addEventListener('click', () => {
+				// 		Gameboard.boardArray = [ "", "", "", "", "", "", "", "", "" ];
+				// 		DisplayController[GameboardDisplay].square.textContent = "";
+				// 		Players.playerOneInput.style.display = "block";
+				// 		Players.playerTwoInput.style.display = "block";
+				// 		DisplayController[SidebarDisplay].playButton.style.display = "block";
+				// 	})
+				})
+				
+				return { sidebar, turnDisplay, playerOneInput, playerOneLabel, playerTwoInput, playerTwoLabel, playButton }	
+				
+			})();
 
 return { SidebarDisplay, GameboardDisplay }
 })();
